6a1646d35e919b9345782bf6e84a82c783407bef
Innup <1139779121@qq.com>
2022-05-10 23:04:48 +0800

    feat(OpenGLES学习)1、详解VBO，把CPU中的数据传到GPU中去

--- app/src/main/cpp/Sence.cpp
+++ app/src/main/cpp/Sence.cpp
@@ -35,13 +35,18 @@ Java_com_innup_learnopengles220414_MainActivity_onSurfaceCreated(JNIEnv *env, jo
     vertices[2].mPosition[2] = -2.0f;
     vertices[2].mPosition[3] = 1.0f;
 
+    GLuint vbo; //vertex buffer object：放顶点缓冲区的一个对象。对硬件来说，数字就是一个明确的东西，所以使用 GLuint 类型。
+    glGenBuffers(1, &vbo);//向显卡申请一个 vbo，即在显卡上创建了一个vbo，并把vbo编号赋值到了我们转入的GLuint中。
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);//把 vbo 设置到 GL_ARRAY_BUFFER 卡槽上去。
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)*3, vertices, GL_STATIC_DRAW);//这里就会把CPU中的数据vertices，传到了GPU的vbo中去。因为此时OpenGL的GL_ARRAY_BUFFER卡槽指向的是刚刚申请的vbo。
+    glBindBuffer(GL_ARRAY_BUFFER, 0);//GL_ARRAY_BUFFER 卡槽重新设置到 0 上去，避免后面在 GL_ARRAY_BUFFER 卡槽上的操作污染 我们上面申请的 vbo。
 
-    int fileSize = 0;
-    unsigned char * fileContent = LoadFileContent(sAssetManager, "test.txt", fileSize);
-    if(fileContent != nullptr){
-        __android_log_print(ANDROID_LOG_INFO, ALICE_LOG_TAG, "[%s]", (char *)fileContent);
-        delete [] fileContent;
-    }
+//    int fileSize = 0;
+//    unsigned char * fileContent = LoadFileContent(sAssetManager, "test.txt", fileSize);
+//    if(fileContent != nullptr){
+//        __android_log_print(ANDROID_LOG_INFO, ALICE_LOG_TAG, "[%s]", (char *)fileContent);
+//        delete [] fileContent;
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL
