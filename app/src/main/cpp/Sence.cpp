77409a39e4e90d3138007a543b928371b7ce8f11
Innup <1139779121@qq.com>
2022-06-07 22:08:10 +0800

    feat(OpenGLES学习)1、正交投影与2D元素绘制

--- app/src/main/cpp/Sence.cpp
+++ app/src/main/cpp/Sence.cpp
@@ -28,10 +28,10 @@ Java_com_innup_learnopengles220414_MainActivity_onSurfaceCreated(JNIEnv *env, jo
     sAssetManager = AAssetManager_fromJava(env, am);
     glClearColor(0.1f, 0.4f, 0.6f, 1.0f);
     //定义三个顶点（每个顶点有四个坐标），此时定位的数据还在 CPU 上。
-    Vertice vertices[3];
+    Vertice vertices[4];
     //设置第一个顶点数据
-    vertices[0].mPosition[0] = -0.5f;
-    vertices[0].mPosition[1] = -0.5f;
+    vertices[0].mPosition[0] = -50.0f;
+    vertices[0].mPosition[1] = -50.0f;
     vertices[0].mPosition[2] = 0.0f;
     vertices[0].mPosition[3] = 1.0f;
     vertices[0].mColor[0] = 1.0f;
@@ -39,8 +39,8 @@ Java_com_innup_learnopengles220414_MainActivity_onSurfaceCreated(JNIEnv *env, jo
     vertices[0].mColor[2] = 0.0;
     vertices[0].mColor[3] = 1.0f;
     //设置第二个顶点数据
-    vertices[1].mPosition[0] = 0.5f;
-    vertices[1].mPosition[1] = -0.5f;
+    vertices[1].mPosition[0] = 50.0f;
+    vertices[1].mPosition[1] = -50.0f;
     vertices[1].mPosition[2] = 0.0f;
     vertices[1].mPosition[3] = 1.0f;
     vertices[1].mColor[0] = 1.0f;
@@ -48,18 +48,27 @@ Java_com_innup_learnopengles220414_MainActivity_onSurfaceCreated(JNIEnv *env, jo
     vertices[1].mColor[2] = 1.0;
     vertices[1].mColor[3] = 1.0f;
     //设置第三个顶点数据
-    vertices[2].mPosition[0] = 0.0f;
-    vertices[2].mPosition[1] = 0.5f;
+    vertices[2].mPosition[0] = -50.0f;
+    vertices[2].mPosition[1] = 50.0f;
     vertices[2].mPosition[2] = 0.0f;
     vertices[2].mPosition[3] = 1.0f;
     vertices[2].mColor[0] = 0.0f;
     vertices[2].mColor[1] = 1.0f;
     vertices[2].mColor[2] = 1.0;
     vertices[2].mColor[3] = 1.0f;
+    //设置第四个顶点数据
+    vertices[3].mPosition[0] = 50.0f;
+    vertices[3].mPosition[1] = 50.0f;
+    vertices[3].mPosition[2] = 0.0f;
+    vertices[3].mPosition[3] = 1.0f;
+    vertices[3].mColor[0] = 0.0f;
+    vertices[3].mColor[1] = 1.0f;
+    vertices[3].mColor[2] = 1.0;
+    vertices[3].mColor[3] = 1.0f;
 
     glGenBuffers(1, &vbo);//向显卡申请一个 vbo，即在显卡上创建了一个vbo，并把vbo编号赋值到了我们转入的GLuint中。
     glBindBuffer(GL_ARRAY_BUFFER, vbo);//把 vbo 设置到 GL_ARRAY_BUFFER 卡槽上去。
-    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)*3, vertices, GL_STATIC_DRAW);//这里就会把CPU中的数据vertices，传到了GPU的vbo中去。因为此时OpenGL的GL_ARRAY_BUFFER卡槽指向的是刚刚申请的vbo。
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices)*4, vertices, GL_STATIC_DRAW);//这里就会把CPU中的数据vertices，传到了GPU的vbo中去。因为此时OpenGL的GL_ARRAY_BUFFER卡槽指向的是刚刚申请的vbo。
     glBindBuffer(GL_ARRAY_BUFFER, 0);//GL_ARRAY_BUFFER 卡槽重新设置到 0 上去，避免后面在 GL_ARRAY_BUFFER 卡槽上的操作污染 我们上面申请的 vbo。
 
     program = CreateStandardProgram(sAssetManager, "test.vs", "test.fs");
@@ -86,15 +95,22 @@ Java_com_innup_learnopengles220414_MainActivity_onSurfaceChanged(JNIEnv *env, jo
     __android_log_print(ANDROID_LOG_INFO, ALICE_LOG_TAG, "onSurfaceChanged width:%d,height:%d", width, height);
     glViewport(0, 0, width, height);
 
-    //设置模型矩阵。将三角形往后推两个单位，再缩放0.5倍，再绕Z轴旋转30度
-    modelMatrix = glm::translate(0.0f, 0.0f, -2.0f) * glm::scale(0.5f, 0.5f, 0.5f)
-                        * glm::rotate(30.0f, 1.0f, 0.0f, 0.0f);
+    //设置模型矩阵。将三角形往后推两个单位
+    modelMatrix = glm::translate(0.0f, 0.0f, -2.0f);
+
+//    //设置模型矩阵。将三角形往后推两个单位，再缩放0.5倍，再绕Z轴旋转30度
+//    modelMatrix = glm::translate(0.0f, 0.0f, -2.0f) * glm::scale(0.5f, 0.5f, 0.5f)
+//                  * glm::rotate(30.0f, 1.0f, 0.0f, 0.0f);
     //设置视口矩阵。放摄像机位置
     viewMatrix=glm::lookAt(glm::vec3(0.0f,0.0f,0.0f),
                            glm::vec3(0.0,0.0,-1.0f),
                            glm::vec3(0.0,1.0f,0.0f));
-    //设置投影矩阵。参数：视角、画布宽与高的比、最近可以看到的距离、最远可以看到的距离
-    projectionMatrix=glm::perspective(45.0f,float(width)/float(height),0.1f,1000.0f);
+//    //设置投影矩阵为透视投影。参数：视角、画布宽与高的比、最近可以看到的距离、最远可以看到的距离
+//    projectionMatrix=glm::perspective(45.0f,float(width)/float(height),0.1f,1000.0f);
+    //设置投影矩阵为正交投影，设置了之后，就可以按照屏幕坐标去绘制了，不用去计算3D位置了。
+    float half_width = float(width) / 2.0f;
+    float half_height = float(height) / 2.0f;
+    projectionMatrix=glm::ortho(-half_width, half_width, -half_height, half_height, 0.1f, 1000.0f);
 }
 extern "C" JNIEXPORT void JNICALL
 Java_com_innup_learnopengles220414_MainActivity_onDrawFrame(JNIEnv *env, jobject thiz) {
@@ -114,7 +130,7 @@ Java_com_innup_learnopengles220414_MainActivity_onDrawFrame(JNIEnv *env, jobject
     glEnableVertexAttribArray(attrColorLocation);
     glVertexAttribPointer(attrColorLocation, 4, GL_FLOAT, GL_FALSE, sizeof(Vertice), (void *)(sizeof(float )*4));
     //从0号点开始画，画3个点
-    glDrawArrays(GL_TRIANGLES, 0, 3);
+    glDrawArrays(GL_TRIANGLE_STRIP, 0, 4);
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glUseProgram(0);
 }
